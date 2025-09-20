3815522191f86491c9027e1eac8bd2035f331f97
uninvitedCalamity <uninvitedCalamity@gmail.com>
2025-09-20 13:19:34 +0100

    Added Subtract_Mix functionality, Removed most repetitive Info logs

--- invert_armature.py
+++ invert_armature.py
@@ -115,6 +115,70 @@ def get_evaluated(originalObject):
     mesh_eval = evaled.to_mesh().vertices
     return mesh_eval  
 
+class generate_refshape(bpy.types.Operator):
+    bl_idname = "object.generate_refshape"
+    bl_label = "Generate Reference Shape"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    def execute(self, context):
+        def Info(string):
+            self.report({"INFO"},str(string))
+
+        invert_tool = context.scene.invert_tool
+        originalObject = bpy.data.objects[invert_tool.target_object]
+        bpy.ops.object.select_all(action='DESELECT')
+        originalObject.select_set(True)
+        context.view_layer.objects.active = originalObject
+        was_original = True
+        Info("Making Copy")
+        bpy.ops.object.duplicate_move(
+            OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, 
+            TRANSFORM_OT_translate={
+                "value":(0, 0, 0), 
+                "orient_type":'GLOBAL', 
+                "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), 
+                "orient_matrix_type":'GLOBAL', 
+                "constraint_axis":(False, False, False), 
+                "mirror":True, 
+                "use_proportional_edit":False, 
+                "proportional_edit_falloff":'SMOOTH', 
+                "proportional_size":1, 
+                "use_proportional_connected":False, 
+                "use_proportional_projected":False, 
+                "snap":False, 
+                "snap_target":'CLOSEST', 
+                "snap_point":(0, 0, 0), 
+                "snap_align":False, 
+                "snap_normal":(0, 0, 0), 
+                "gpencil_strokes":False, 
+                "cursor_transform":False, 
+                "texture_space":False, 
+                "remove_on_cancel":False, 
+                "release_confirm":False, 
+                "use_accurate":False}
+            )
+        copyObject = context.view_layer.objects.active
+        copyObject.name = originalObject.name + "ReferenceCopy"
+        #Add Conversion to active shape here
+        if(copyObject.data.shape_keys != None):
+            bpy.ops.object.shape_key_add(from_mix=True)
+            shapes_length = len(copyObject.data.shape_keys.key_blocks)
+            shapes_iterator = shapes_length - 1
+            firstRun:bool = True
+            if shapes_length > 0:
+                while shapes_iterator >= 0:
+                    copyObject.active_shape_key_index = shapes_iterator
+                    copyObject.data.shape_keys.key_blocks[shapes_iterator].lock_shape = False
+                    if not firstRun:
+                        bpy.ops.object.shape_key_remove()
+                    shapes_iterator -= 1
+                    firstRun = False
+
+                copyObject.shape_key_clear()
+        for modifier in copyObject.modifiers:
+            if modifier.type == 'ARMATURE':
+                bpy.ops.object.modifier_apply(modifier=modifier.name)
+        return{'FINISHED'}
 
 class invert_armature(bpy.types.Operator):
     """Armature inverter"""
@@ -124,6 +188,8 @@ class invert_armature(bpy.types.Operator):
 
     def execute(self, context):   
         invert_tool = context.scene.invert_tool
+
+        finish_message = 'Finished Inversion'
 
         was_original:bool = False
         def Info(string):
@@ -160,7 +226,6 @@ class invert_armature(bpy.types.Operator):
         originalObject = bpy.data.objects[invert_tool.target_object]
         if(originalObject.data.shape_keys != None):
             ShapeName = originalObject.active_shape_key.name
-            Info(ShapeName)
         copyObject = None
         if invert_tool.reference_object != "":
             copyObject = bpy.data.objects[invert_tool.reference_object]
@@ -208,7 +273,6 @@ class invert_armature(bpy.types.Operator):
             #Add Conversion to active shape here
             if(copyObject.data.shape_keys != None):
                 shapes_length = len(copyObject.data.shape_keys.key_blocks)
-                Info(shapes_length - 1)
                 shapes_iterator = shapes_length - 1
 
                 if shapes_length > 0:
@@ -257,7 +321,6 @@ class invert_armature(bpy.types.Operator):
                 modifyObject.name = ShapeName + "_Modified"
             if(modifyObject.data.shape_keys != None):
                 shapes_length = len(modifyObject.data.shape_keys.key_blocks)
-                Info(shapes_length - 1)
                 shapes_iterator = shapes_length - 1
 
                 if shapes_length > 0:
@@ -278,7 +341,6 @@ class invert_armature(bpy.types.Operator):
             for modifier in modifyObject.modifiers:
                 if modifier.type == 'ARMATURE':
                     modifier.show_viewport = True
-                    Info(modifier)
                     boneDictionary = {}
                     for bone in modifier.object.data.bones:
                         boneDictionary[bone.name] = {"BaseBone":bone}  
@@ -323,19 +385,144 @@ class invert_armature(bpy.types.Operator):
             for modifier in modifyObject.modifiers:
                 if modifier.type == 'ARMATURE':
                     modifier.show_viewport=False
+
+            #SubtractMix
+            mixObject = None
+            if context.scene.invert_tool.subtract_mix and bpy.data.objects[invert_tool.reference_object] is not None:
+                bpy.ops.object.select_all(action='DESELECT')
+                originalObject.select_set(True)
+                context.view_layer.objects.active = originalObject
+                was_original = True
+                Info("Making Copy")
+                bpy.ops.object.duplicate_move(
+                    OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, 
+                    TRANSFORM_OT_translate={
+                        "value":(0, 0, 0), 
+                        "orient_type":'GLOBAL', 
+                        "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), 
+                        "orient_matrix_type":'GLOBAL', 
+                        "constraint_axis":(False, False, False), 
+                        "mirror":True, 
+                        "use_proportional_edit":False, 
+                        "proportional_edit_falloff":'SMOOTH', 
+                        "proportional_size":1, 
+                        "use_proportional_connected":False, 
+                        "use_proportional_projected":False, 
+                        "snap":False, 
+                        "snap_target":'CLOSEST', 
+                        "snap_point":(0, 0, 0), 
+                        "snap_align":False, 
+                        "snap_normal":(0, 0, 0), 
+                        "gpencil_strokes":False, 
+                        "cursor_transform":False, 
+                        "texture_space":False, 
+                        "remove_on_cancel":False, 
+                        "release_confirm":False, 
+                        "use_accurate":False}
+                    )
+                mixObject = context.view_layer.objects.active
+                mixObject.name = originalObject.name + "MixCopy"
+                if(mixObject.data.shape_keys != None):
+                    bpy.ops.object.shape_key_add(from_mix=True)
+                    shapes_length = len(mixObject.data.shape_keys.key_blocks)
+                    shapes_iterator = shapes_length - 1
+                    firstRun:bool = True
+                    if shapes_length > 0:
+                        while shapes_iterator >= 0:
+                            mixObject.active_shape_key_index = shapes_iterator
+                            mixObject.data.shape_keys.key_blocks[shapes_iterator].lock_shape = False
+                            if not firstRun:
+                                bpy.ops.object.shape_key_remove()
+                            shapes_iterator -= 1
+                            firstRun = False
+
+                        mixObject.shape_key_clear()
+                
+                bpy.ops.object.select_all(action='DESELECT')
+                originalObject.select_set(True)
+                context.view_layer.objects.active = originalObject
+                was_original = True
+                Info("Making Copy")
+                bpy.ops.object.duplicate_move(
+                    OBJECT_OT_duplicate={"linked":False, "mode":'TRANSLATION'}, 
+                    TRANSFORM_OT_translate={
+                        "value":(0, 0, 0), 
+                        "orient_type":'GLOBAL', 
+                        "orient_matrix":((1, 0, 0), (0, 1, 0), (0, 0, 1)), 
+                        "orient_matrix_type":'GLOBAL', 
+                        "constraint_axis":(False, False, False), 
+                        "mirror":True, 
+                        "use_proportional_edit":False, 
+                        "proportional_edit_falloff":'SMOOTH', 
+                        "proportional_size":1, 
+                        "use_proportional_connected":False, 
+                        "use_proportional_projected":False, 
+                        "snap":False, 
+                        "snap_target":'CLOSEST', 
+                        "snap_point":(0, 0, 0), 
+                        "snap_align":False, 
+                        "snap_normal":(0, 0, 0), 
+                        "gpencil_strokes":False, 
+                        "cursor_transform":False, 
+                        "texture_space":False, 
+                        "remove_on_cancel":False, 
+                        "release_confirm":False, 
+                        "use_accurate":False}
+                    )
+                originalCopy = context.view_layer.objects.active
+                originalCopy.name += 'OrigngCopy'
+                originalCopy.shape_key_clear()
+                JoinAsShape(originalCopy.name,modifyObject.name)
+                JoinAsShape(originalCopy.name,mixObject.name)
+                originalCopy.data.shape_keys.key_blocks[2].slider_min = -1
+                originalCopy.data.shape_keys.key_blocks[2].value = -1
+                originalCopy.data.shape_keys.key_blocks[1].value = 1
+                bpy.ops.object.shape_key_add(from_mix = True)
+                
+                shapes_length = len(originalCopy.data.shape_keys.key_blocks)
+                shapes_iterator = shapes_length - 1
+                firstRun:bool = True
+                if shapes_length > 0:
+                    while shapes_iterator >= 0:
+                        originalCopy.active_shape_key_index = shapes_iterator
+                        originalCopy.data.shape_keys.key_blocks[shapes_iterator].lock_shape = False
+                        if not firstRun:
+                            bpy.ops.object.shape_key_remove()
+                        shapes_iterator -= 1
+                        firstRun = False
+                bpy.ops.object.select_all(action='DESELECT')
+                mixObject.select_set(True)
+                context.view_layer.objects.active = mixObject
+                bpy.ops.object.delete(use_global=False, confirm=False)
+                bpy.ops.object.select_all(action='DESELECT')
+                modifyObject.select_set(True)
+                context.view_layer.objects.active = modifyObject
+                bpy.ops.object.delete(use_global=False, confirm=False)
+                originalCopy.name = originalObject.name + 'Subtracted_mix'
+                modifyObject = originalCopy
+
+                finish_message = 'Finished Inversion with subtract mix'
+            #SubtractMix end
+
             JoinAsShape(originalObject.name,modifyObject.name)
             for modifier in originalObject.modifiers:
                 if modifier.type == 'ARMATURE':
                     modifier.show_viewport=True
             
+
+
             bpy.ops.object.select_all(action='DESELECT')
             modifyObject.select_set(True)
             if was_original:
                 copyObject.select_set(True)
             context.view_layer.objects.active = modifyObject
             bpy.ops.object.delete(use_global=False, confirm=False)
-
-        Info("FINISHED")
+            originalObject.select_set(True)
+            context.view_layer.objects.active = originalObject
+            shapes_length = len(originalObject.data.shape_keys.key_blocks)
+            shapes_iterator = shapes_length - 1
+            originalObject.data.shape_keys.key_blocks[shapes_iterator].value = 1
+        Info(finish_message)
         return{'FINISHED'}
     
     
@@ -345,6 +532,7 @@ class properties(bpy.types.PropertyGroup):
     step_size : bpy.props.FloatProperty(name= "Max step size", soft_min= 0.000001, default=1)
     min_step_size : bpy.props.FloatProperty(name= "Min step size", soft_min= 0.000001, default=1)
     localise : bpy.props.BoolProperty(name= "localise")
+    subtract_mix: bpy.props.BoolProperty(name = "subtract_mix")
     
 #Object picker based on the object picker from bartoszstyperek's shape transfer tool (https://bartoszstyperek.gumroad.com/)
 class ST_OT_ObjectPicker(bpy.types.Operator):
@@ -387,12 +575,16 @@ class VIEW3D_PT_invert_armature_panel(bpy.types.Panel):
         row.prop(invertTool, "min_step_size")
         row = col.row(align=True)
         row.prop(invertTool, "localise")
+        row = col.row(align=True)
+        row.prop(invertTool, "subtract_mix")
         
         row = self.layout.row()
         row.operator("object.invert_armature", text="Invert Armature")
+        row = self.layout.row()
+        row.operator("object.generate_refshape", text="Generate Reference")
 
 
-classes = [properties, VIEW3D_PT_invert_armature_panel, invert_armature, ST_OT_ObjectPicker]
+classes = [properties, VIEW3D_PT_invert_armature_panel, invert_armature, ST_OT_ObjectPicker, generate_refshape]
 
 def menu_func(self, context):
     self.layout.operator(invert_armature.bl_idname)
